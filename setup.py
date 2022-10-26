4afce2001b4c1b9de56c834841c844fec8733025
Michael Uhl <uhl.michael@gmx.de>
2022-10-26 18:06:38 +0200

    mo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='weasel',
-    version='0.3',
+    version='0.4',
     description='Enter the weasel',
     long_description=open('README.md').read(),
     author='Michael Uhl',
